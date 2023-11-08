d40616881c832ec430c9edc93d75e55a174a1ca1
phuoc <phuocb2014601@student.ctu.edu.vn>
2023-11-08 15:46:52 +0700

    update product.php

--- app/controllers/product.php
+++ app/controllers/product.php
@@ -50,33 +50,36 @@ class product extends DController{
     public function insert_recipe(){
         $title = $_POST['title_recipe'];
         $desc = $_POST['desc_recipe'];
-        $img = $_FILES['img_recipe']['name'];
-        $tmp_img = $_FILES['img_recipe']['tmp_name'];
+
         $ingredient = $_POST['ingredient'];
         $step = $_POST['steptodo'];
+
+        $img = $_FILES['img_recipe']['name'];
+        $tmp_image = $_FILES['img_recipe']['tmp_name'];
         
         $div = explode('.', $img);
         $file_ext = strtolower(end($div));
-        $unique_img = $div[0].time().'.'.$file_ext;
+        $unique_image = $div[0].time().'.'.$file_ext;
 
-        $path_upload = "public/upload/product/".$unique_img;
-
-
+        $path_uploads = "public/uploads/product/".$unique_image;
 
         $category = $_POST['category'];
+
         $table = "recipe";
+    
         $data = array(
             'title_recipe' => $title,
             'desc_recipe' => $desc,
-            'img_recipe' => $unique_img,
-            'id_category' => $category,
             'ingredient' => $ingredient,
-            'steptodo' => $step
+            'steptodo' => $step,
+            'img_recipe' => $unique_image,
+            'id_category' => $category
         );
 
         $categorymodel = $this->load->model('categorymodel');
         $result = $categorymodel->insert_recipe($table, $data);
         if ($result==1){
+            move_uploaded_file($tmp_image, $path_uploads);
             $message['msg']= "Thêm công thức thành công";
             header('Location:'.BASE_URL."/product/add_recipe?msg=".urlencode(serialize($message)));
         }else{
@@ -87,6 +90,20 @@ class product extends DController{
        
     }
 
+    public function list_recipe(){
+        $this->load->view('cpanel/header');
+        $this->load->view('cpanel/navbar');
+
+        $table_recipe = "recipe";
+        $table_category = "category";
+
+        $categorymodel = $this->load->model('categorymodel');
+        $data['recipe'] = $categorymodel->recipe($table_recipe, $table_category);
+
+
+        $this->load->view('cpanel/product/list_recipe',$data);
+        $this->load->view('cpanel/footer');
+    }
 
     public function list_category(){
         $this->load->view('cpanel/header');
@@ -115,6 +132,21 @@ class product extends DController{
         }
     }
 
+    public function delete_recipe($id){
+        $table = "recipe";
+        $cond = "id_recipe='$id'";
+        $categorymodel = $this->load->model('categorymodel');
+        $result = $categorymodel->delete_recipe($table,$cond);
+        if($result==1){
+            $message['msg']= "Xóa công thức món ăn thành công";
+            header('Location:'.BASE_URL."/product/list_recipe?msg=".urlencode(serialize($message)));
+        }else{
+            $message['msg']= "Xóa công thức món ăn thất bại";
+            header('Location:'.BASE_URL."/product/list_recipe?msg=".urlencode(serialize($message)));
+
+        }
+    }
+
     public function edit_category($id){
         $table = "category";
         $cond = "id_category='$id'";
@@ -123,6 +155,21 @@ class product extends DController{
         $this->load->view('cpanel/header');
         $this->load->view('cpanel/navbar');
         $this->load->view('cpanel/product/edit_category', $data);
+        $this->load->view('cpanel/footer');
+    }
+
+    public function edit_recipe($id){
+        $table_recipe = "recipe";
+        $table_category = "category";
+        $cond = "id_recipe='$id'";
+        $categorymodel = $this->load->model('categorymodel');
+
+        $data['recipebyid'] = $categorymodel->recipebyid($table_recipe,$cond);
+        $data['category'] = $categorymodel->category($table_category);
+        
+        $this->load->view('cpanel/header');
+        $this->load->view('cpanel/navbar');
+        $this->load->view('cpanel/product/edit_recipe', $data);
         $this->load->view('cpanel/footer');
     }
 
@@ -144,6 +191,66 @@ class product extends DController{
         }else{ 
             $message['msg']= "Cập nhật danh mục món ăn thất bại";
             header('Location:'.BASE_URL."/product/list_category?msg=".urlencode(serialize($message)));
+
+        }
+    }
+
+    public function update_recipe($id){
+        $table = "recipe";
+        $cond = "id_recipe='$id'";
+        $categorymodel = $this->load->model('categorymodel');
+
+        $title = $_POST['title_recipe'];
+        $desc = $_POST['desc_recipe'];
+        $ingredient = $_POST['ingredient'];
+        $step = $_POST['steptodo'];
+        $category = $_POST['category'];
+
+        $img = $_FILES['img_recipe']['name'];
+        $tmp_image = $_FILES['img_recipe']['tmp_name'];
+        
+        $div = explode('.', $img);
+        $file_ext = strtolower(end($div));
+        $unique_image = $div[0].time().'.'.$file_ext;
+
+        $path_uploads = "public/uploads/product/".$unique_image;
+
+        if($img){
+            $data['recipebyid'] = $categorymodel->recipebyid($table,$cond);
+            foreach($data['recipebyid'] as $key =>$value){
+                
+                    $path_unlink= "public/uploads/product/";
+                    unlink($path_unlink.$value['img_recipe']);
+            }
+            
+            $data = array(
+                'title_recipe' => $title,
+                'desc_recipe' => $desc,
+                'ingredient' => $ingredient,
+                'steptodo' => $step,
+                'img_recipe' => $unique_image,
+                'id_category' => $category
+            );
+            move_uploaded_file($tmp_image, $path_uploads);
+        }else{
+            $data = array(
+                'title_recipe' => $title,
+                'desc_recipe' => $desc,
+                'ingredient' => $ingredient,
+                'steptodo' => $step,
+                //'img_recipe' => $unique_image,
+                'id_category' => $category
+            );
+        }
+        
+
+        $result = $categorymodel->update_recipe($table, $data, $cond);
+        if ($result==1){
+            $message['msg']= "Cập nhật công thức thành công";
+            header('Location:'.BASE_URL."/product/list_recipe?msg=".urlencode(serialize($message)));
+        }else{
+            $message['msg']= "Cập nhật công thức thất bại";
+            header('Location:'.BASE_URL."/product/list_recipe?msg=".urlencode(serialize($message)));
 
         }
     }
