d44c7a6a879b65ee3f03f183857e55deaef027d9
Truong Ngoc Vien <vienb2014630@student.ctu.edu.vn>
2023-11-08 15:32:45 +0700

    categorymodel

--- app/models/categorymodel.php
+++ app/models/categorymodel.php
@@ -31,6 +31,27 @@ class categorymodel extends DModel{
         return $this->db->insert($table, $data);
     }
 
+    
+
+    public function recipe($table_recipe, $table_category){
+        $sql = "SELECT * FROM $table_recipe, $table_category WHERE $table_recipe.id_category = $table_category.id_category ORDER BY $table_recipe.id_recipe DESC";
+        return $this->db->select($sql);
+        
+    }
+
+    public function delete_recipe($table_recipe,$cond){
+        return $this->db->delete($table_recipe, $cond);
+    }
+
+    public function recipebyid($table,$cond){
+        $sql = "SELECT * FROM $table WHERE $cond";
+        return $this->db->select($sql);
+    }
+    
+    public function update_recipe($category,$data,$cond){
+        return $this->db->update($category,$data,$cond);
+    }
+
 }
 ?>
 
